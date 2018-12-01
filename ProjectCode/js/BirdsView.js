e41dde1aca6bca7dc97a235fb759ffedc896c3bc
breeder88 <brettreeder63@hotmail.com>
2018-11-30 22:20:24 -0700

    fixed home team wins plot

--- ProjectCode/js/BirdsView.js
+++ ProjectCode/js/BirdsView.js
@@ -13,6 +13,7 @@ class BirdsView{
 			"Royal Challengers Bangalore":["Bangalore"],
 		};
 		this.codes = {
+			"":"",
 			"Sunrisers Hyderabad":"SRH",
 			"Chennai Super Kings":"CSK",
 			"Delhi Daredevils":"DD",
@@ -48,8 +49,8 @@ class BirdsView{
 			   .attr("id","supportTooltip");
 	}
 	deriveData(){
-		console.log("hometownData: ",this.hometownData);
-		console.log("rankings: ",this.rankings);
+		// console.log("hometownData: ",this.hometownData);
+		// console.log("rankings: ",this.rankings);
 		var teams = [];
 		for(var row of this.hometownData){
 			teams = teams.concat(row["winner"]);
@@ -58,12 +59,12 @@ class BirdsView{
 		for(var row of this.hometownData){
 			cities = cities.concat(row["city"]);
 		}
-		console.log("teams: ",teams);
-		console.log("cities: ",cities);
+		// console.log("teams: ",teams);
+		// console.log("cities: ",cities);
 		var dTeams = [...new Set(teams)];
-		console.log("distinct teams: ",dTeams);
+		// console.log("distinct teams: ",dTeams);
 		var dCities = [...new Set(cities)];
-		console.log("distinct cities: ",dCities);
+		// console.log("distinct cities: ",dCities);
 		var data = [];
 		for(let team of dTeams){
 			let teamData = this.hometownData.filter(d => d["winner"]===team);
@@ -71,7 +72,7 @@ class BirdsView{
 			for(let data of teamData){
 				totalGames += +data["count"];
 			}
-			console.log(totalGames);
+			// console.log(totalGames);
 			//filter homegames 
 			var won = 0;
 			for(let home of this.hometowns[team]){
@@ -81,39 +82,77 @@ class BirdsView{
 			}
 			var lost = totalGames - won;
 			var obj = {
+				"code":this.codes[team],
 				"team":team,
 				"won":won,
 				"lost":lost,
 			};
 			data = data.concat(obj);
 		}
-		console.log("data: ",data);
+		// console.log("data: ",data);
 		return data;
 
 	}
 	createComparisionChart(){
 		let tooltip = d3.select("#supportTooltip");
 		var data = this.deriveData();
-		console.log("data: ",data);
+		//console.log("data: ",data);
 		var xScale = d3.scalePoint()
 					   .domain(Object.values(this.codes))
-					   .range([0,this.svgWidth])
-					   //.padding(2);
+					   .range([0,this.svgWidth-100]);
 		var yScale = d3.scaleLinear()
-					   .domain([0,100])
-					   .range([this.svgHeight,0]);
+					   .domain(["0","100"])
+					   .range([this.svgHeight,100])
+					   .nice();
 	    var xAxis = d3.axisBottom();
-	    var yAxis = d3.axisLeft();
+		var yAxis = d3.axisLeft();
+		xAxis.scale(xScale);
+	    yAxis.scale(yScale);
+		this.svg.append("g")
+    		 .attr("transform", "translate(50,450)")
+    		 .attr("class","axis")
+    		 .call(xAxis);
+    
+		this.svg.append("g")
+    		 .attr("transform", "translate(50,-50)")
+    		 .attr("class","axis")
+			 .call(yAxis);
+			 
+		console.log(yScale(31))
+		var lostG = this.svg.append('g')
+	    				.attr("transform","translate(50,"+this.svgHeight+") scale(1,-1)")
+	    var lostRects = lostG.selectAll("rect")
+					        .data(data)
+					        .enter()
+					        .append("rect")
+							.attr("x",(d)=>{
+								return xScale(d.code)-10
+							   })
+	         				.attr("y",d=>50)
+	         				.attr("width",d=>20)
+	         				.attr("height",d=>this.svgHeight-yScale(d.won+d.lost))
+	         				.attr("fill","#6ab5fc")
+	         				.on("mouseover",d => {
+                   				 tooltip.html(this.tooltipRender(d))
+                           				.style("left", d3.event.pageX-800 + "px")
+                           				.style("top", d3.event.pageY-200 + "px")
+                           				.style("opacity", 1);
+	         				})
+	         				.on("mouseout",d => {
+	         					tooltip.style("opacity",0);
+	         				});
 	    var wonG = this.svg.append('g')
 	    				.attr("transform","translate(50,"+this.svgHeight+") scale(1,-1)")
 	    var wonRects = wonG.selectAll("rect")
 					        .data(data)
 					        .enter()
 					        .append("rect")
-	         				.attr("x",(d,i)=>i*50+20)
+	         				.attr("x",(d)=>{
+								 return xScale(d.code)-10
+								})
 	         				.attr("y",d=>50)
 	         				.attr("width",d=>20)
-	         				.attr("height",d=>yScale(d.won))
+	         				.attr("height",d=>this.svgHeight-yScale(d.won))
 	         				.attr("fill","#d12525")
 	         				.on("mouseover",d => {
                    				 tooltip.html(this.tooltipRender(d))
@@ -125,43 +164,13 @@ class BirdsView{
 	         					tooltip.style("opacity",0);
 	         				});
 
-	    var lostG = this.svg.append('g')
-	    				.attr("transform","translate(50,"+this.svgHeight+") scale(1,-1)")
-	    var lostRects = lostG.selectAll("rect")
-					        .data(data)
-					        .enter()
-					        .append("rect")
-	         				.attr("x",(d,i)=>i*50+20)
-	         				.attr("y",d=>50)
-	         				.attr("width",d=>20)
-	         				.attr("height",d=>yScale(d.won+d.lost))
-	         				.attr("fill","#6ab5fc")
-	         				.on("mouseover",d => {
-                   				 tooltip.html(this.tooltipRender(d))
-                           				.style("left", d3.event.pageX-800 + "px")
-                           				.style("top", d3.event.pageY-200 + "px")
-                           				.style("opacity", 1);
-	         				})
-	         				.on("mouseout",d => {
-	         					tooltip.style("opacity",0);
-	         				});
-	    xAxis.scale(xScale);
-	    yAxis.scale(yScale);
+	    
 	    var margin = 30;
-	    this.svg.append("g")
-    		 .attr("transform", "translate(50,450)")
-    		 .attr("class","axis")
-    		 .call(xAxis);
-    
-		this.svg.append("g")
-    		 .attr("transform", "translate(50,-50)")
-    		 .attr("class","axis")
-    		 .call(yAxis);
 
 		
 	}
 	    tooltipRender(data){
-	    	console.log(data);
+	    	//console.log(data);
 	    	return "<h3>"+data["team"]+""+"<p> Won: "+data["won"]
 	    	+"</p><p>Lost: "+data["lost"]+"</p></h3>";
 
